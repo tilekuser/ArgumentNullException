cf1792d26c050c1d106e791a95b856bd2f1cc291
tilekuser <67508428+tilekuser@users.noreply.github.com>
2020-08-08 22:56:48 +0600

    Add files via upload

---
+++ Exception/Exception/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student("", "Kasymov", "2");
+            student.Print();
+        }
+    }
+}
---
+++ Exception/Exception/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exception
+{
+    class Student
+    {
+        string Name;
+        string Surname;
+        string Cours;
+        public Student(string name, string surname, string cours)
+        {
+            Name = name;
+            Surname = surname;
+            Cours = cours;
+        }
+        public void Print()
+        {
+            try
+            {
+                Console.WriteLine($"Name: {Name}\nSurname; {Surname}\nCours: {Cours}");
+            }
+            finally
+            {
+                if (string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(Surname))
+                {
+                    throw new ArgumentNullException("Name and surname cannot be null or empty.");
+                }
+            }
+        }
+    }
+}
